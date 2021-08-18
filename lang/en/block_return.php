55c357b98335f719e81d2dfef001ad4fb9f6f236
lchamney <lchamney@gmail.com>
2021-08-18 11:26:05 -0400

    Create block_return.php

---
+++ lang/en/block_return.php
@@ -0,0 +1,9 @@
+<?php
+$string['pluginname'] = 'Return block';
+$string['return'] = 'Return';
+$string['click_to_continue'] = 'Click to continue where you left off.';
+$string['returnl:addinstance'] = 'Add a new return block';
+$string['return:myaddinstance'] = 'Add a new return block to the My Moodle page';
+$string['lightbox'] = 'Display as a lightbox at the bottom of the page.';
+
+?>
