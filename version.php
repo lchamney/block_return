9dec3387b9fb552c20f6d36855e80b50e140dbeb
lchamney <lchamney@gmail.com>
2021-08-18 11:34:29 -0400

    Update version.php

--- version.php
+++ version.php
@@ -1,5 +1,6 @@
 <?php
 $plugin->component = 'block_return';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
+$plugin->release = "1.0.0";
 $plugin->version = 2021070700;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 
