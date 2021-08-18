207400981d279cf2af42d5c4502a70ffb2f5f9da
lchamney <lchamney@gmail.com>
2021-08-18 11:27:03 -0400

    Create block_return.php

---
+++ lang/fr_ca/block_return.php
@@ -0,0 +1,7 @@
+<?php
+$string['pluginname'] = 'Return block';
+$string['return'] = 'Return';
+$string['click_to_continue'] = 'Cliquez pour continuer là où vous vous étiez arrêté.';
+$string['returnl:addinstance'] = 'Add a new return block';
+$string['return:myaddinstance'] = 'Add a new return block to the My Moodle page';
+?>
