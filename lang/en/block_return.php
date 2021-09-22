c151b622252067bd465d9d04c4355e31af085855
lchamney <lchamney@gmail.com>
2021-09-22 15:10:01 -0400

    Update block_return.php

--- lang/en/block_return.php
+++ lang/en/block_return.php
@@ -1,9 +1,35 @@
 <?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for editing HTML block instances.
+ *
+ * @package    block_return
+ * @copyright  2021 Lee Chamney, Commissionaires Ottawa
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['pluginname'] = 'Return block';
 $string['return'] = 'Return';
 $string['click_to_continue'] = 'Click to continue where you left off.';
 $string['returnl:addinstance'] = 'Add a new return block';
 $string['return:myaddinstance'] = 'Add a new return block to the My Moodle page';
 $string['lightbox'] = 'Display as a lightbox at the bottom of the page.';
+$string['privacy:metadata'] = 'The Return block retrieves a database entry containing the user's last-visited activity, though not the timestamp or specific actions taken, and uses it to format a link for display. This entry is not stored after the user has navigated away.';
+
 
 ?>
