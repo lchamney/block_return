26b5cad96328c3d7b6cb8d55292011619bbb2c7d
lchamney <lchamney@gmail.com>
2021-09-22 15:11:19 -0400

    Update block_return.php

--- lang/en/block_return.php
+++ lang/en/block_return.php
@@ -25,7 +25,7 @@
 
 $string['pluginname'] = 'Return block';
 $string['return'] = 'Return';
-$string['click_to_continue'] = 'Click to continue where you left off.';
+$string['click_to_continue'] = 'Click to return to your last-accessed activity.';
 $string['returnl:addinstance'] = 'Add a new return block';
 $string['return:myaddinstance'] = 'Add a new return block to the My Moodle page';
 $string['lightbox'] = 'Display as a lightbox at the bottom of the page.';
