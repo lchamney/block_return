c6b0f4e76bfb5ed173e99430b33aaba8536e09a3
lchamney <lchamney@gmail.com>
2021-09-22 14:55:23 -0400

    Delete block_return.php

--- lang/fr_ca/block_return.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['pluginname'] = 'Return block';
-$string['return'] = 'Return';
-$string['click_to_continue'] = 'Cliquez pour continuer là où vous vous étiez arrêté.';
-$string['returnl:addinstance'] = 'Add a new return block';
-$string['return:myaddinstance'] = 'Add a new return block to the My Moodle page';
-?>
