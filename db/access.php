41ec8237676c46de2239064ff91a36a1e7e0a045
lchamney <lchamney@gmail.com>
2022-06-15 15:11:45 -0400

    Update access.php

--- db/access.php
+++ db/access.php
@@ -1,6 +1,5 @@
 <?php
-
- // This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -22,7 +21,7 @@
  * @copyright  2021 Lee Chamney, Commissionaires Ottawa
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-    $capabilities = array(
+ $capabilities = array(
  
     'block/return:myaddinstance' => array(
         'captype' => 'write',
